471737eab012cbb27f11da3fb4ebbd325978bd61
unknown <muktinathrajbanshi786@gmail.com>
2026-03-14 21:13:13 +0545

    wow siddhina lago man

--- index.js
+++ index.js
@@ -15,3 +15,53 @@ list.innerHTML += `
 <td>${book.status}</td>
 
 <td>
+
+<button class="issue" onclick="issueBook(${index})">Issue</button>
+
+<button class="return" onclick="returnBook(${index})">Return</button>
+
+<button class="delete" onclick="deleteBook(${index})">Delete</button>
+
+</td>
+
+</tr>
+`;
+
+});
+
+localStorage.setItem("books",JSON.stringify(books));
+
+}
+
+function addBook(){
+
+let title = document.getElementById("title").value;
+let author = document.getElementById("author").value;
+let id = document.getElementById("id").value;
+
+if(title=="" || author=="" || id==""){
+alert("Please fill all fields");
+return;
+}
+
+books.push({
+id:id,
+title:title,
+author:author,
+status:"Available"
+});
+
+displayBooks();
+
+document.getElementById("title").value="";
+document.getElementById("author").value="";
+document.getElementById("id").value="";
+
+}
+
+function issueBook(index){
+
+if(books[index].status=="Issued"){
+alert("Book already issued");
+return;
+}
